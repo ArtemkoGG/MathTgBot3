c371c7d0bc07758026dc43e7494ed2dda888216f
timib <timibedryk5@gmail.com>
2025-03-03 18:21:49 +0200

    second

--- perimetr.py
+++ perimetr.py
@@ -2,45 +2,36 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 from commands import PERIMETR
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from requestform import Form
+from random import randint
+from aiogram import F, Router
+from aiogram.filters import Command
+from aiogram.filters.callback_data import CallbackQuery
+from aiogram.types import InlineKeyboardButton, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 router = Router()
 
 
 @router.message(Command(PERIMETR))
 async def perimetr(message: Message, state: FSMContext):
     await state.set_state(Form.parameters)
-    await message.answer("Периметр якої фігури в бажаєте визначити?\n 1 - Трикутник\n 2 - Квадрат\n 3 - Прямокутник\n 4 - Коло")
+    await message.answer("Периметр якої фігури в бажаєте визначити?")
+
+def inline_learn_keyboard():
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        InlineKeyboardButton(
+            text = "Трикутник"
+        )
+    )
+
+    builder.row(InlineKeyboardButton(text="Трикутник", callback_data="value_random"))
+    builder.add(InlineKeyboardButton(text="10", callback_data="value_10"))
+
+    return builder.as_markup()
 
 @router.message(Form.parameters)
 async def solve_perimetr(message: Message, state: FSMContext):
     await state.update_data(parameters=message.text)
-    data: dict[str, str] = await state.get_data()
-
-    choice = data["parameters"]
-
-    if choice == "1":
-        await state.set_state(Form.parameters)
-        await message.answer("Введіть 3 сторони трикутника через пробіл (a b c):")
-        coefficients = data["parameters"].split()
-        a = float(coefficients[0])
-        b = float(coefficients[1])
-        c = float(coefficients[2])
-
-        perimetr_triangle = a + b + c
-        await message.answer(f"Ось периметр трикутника {perimetr_triangle}")
-
-    elif choice == "2":
-        await state.set_state(Form.parameters)
-        await message.answer("Введіть довжину сторони квадрата:")
-    elif choice == "3":
-        await state.set_state(Form.parameters)
-        await message.answer("Введіть довжини двох сторін прямокутника через пробіл (a b):")
-    elif choice == "4":
-        await state.set_state(Form.parameters)
-        await message.answer("Введіть радіус кола:")
-    else:
-        await message.answer("Невірний вибір. Будь ласка, виберіть 1, 2, 3 або 4.")
-
-
